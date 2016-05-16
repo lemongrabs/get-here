093a48ce809448336ea6a08420059832c645abf0
Zane Shelby <zaneshelby@gmail.com>
2016-05-15 23:49:02 -0400

    Add mount

--- project.clj
+++ project.clj
@@ -1,17 +1,25 @@
 (defproject get-here "0.1.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/data.csv "0.1.2"]
+                 [org.clojure/core.memoize "0.5.6"]
+
                  [clj-time "0.7.0"]
                  [aysylu/loom "0.4.2"]
+
+                 ;; serialization
+                 [cheshire "5.3.1"]
+                 [org.clojure/data.csv "0.1.2"]
+                 [org.clojure/data.json "0.2.6"]
+
+                 ;; web
                  [ring "1.3.0"]
                  [ring/ring-jetty-adapter "1.3.0"]
                  [ring-middleware-format "0.5.0"]
-                 [cheshire "5.3.1"]
                  [compojure "1.1.8"]
-                 [org.clojure/core.memoize "0.5.6"]
                  [http-kit "2.1.18"]
+
+                 ;;
                  [environ "1.0.0"]
-                 [org.clojure/data.json "0.2.6"]]
+                 [mount "0.1.10"]]
 
   :plugins [[lein-watch "0.0.2"]
             [lein-haml-sass "0.2.7-SNAPSHOT"]
@@ -22,27 +30,27 @@
          :output-extension "css"}
 
   :minify-assets
-    {:assets
-      {"resources/static/public/app.min.css" ["resources/static/vendor"
-                                              "resources/static/css"]
-       "resources/static/public/app.min.js"  ["resources/static/vendor/jquery.min.js"
-                                              "resources/static/vendor/bootstrap"
-                                              "resources/static/vendor/datepicker"
-                                              "resources/static/vendor/underscore.min.js"
-                                              "resources/static/vendor/react.min.js"
-                                              "resources/static/vendor/transit-0.8.807.js"
-                                              "resources/static/vendor/moment.min.js"
-                                              "resources/static/js"]}}
+  {:assets
+   {"resources/static/public/app.min.css" ["resources/static/vendor"
+                                           "resources/static/css"]
+    "resources/static/public/app.min.js"  ["resources/static/vendor/jquery.min.js"
+                                           "resources/static/vendor/bootstrap"
+                                           "resources/static/vendor/datepicker"
+                                           "resources/static/vendor/underscore.min.js"
+                                           "resources/static/vendor/react.min.js"
+                                           "resources/static/vendor/transit-0.8.807.js"
+                                           "resources/static/vendor/moment.min.js"
+                                           "resources/static/js"]}}
 
   :watch {
-    :rate 500
-    :watchers {
-      :scss {:watch-dirs ["resources/static/scss" "resources/static/vendor"]
-             :file-patterns [#"\.scss"]
-             :tasks ["scss once" "minify-assets"]}
-      :js   {:watch-dirs ["resources/static/js" "resources/static/vendor"]
-             :file-patterns [#"\.js"]
-             :tasks ["minify-assets"]}}}
+          :rate 500
+          :watchers {
+                     :scss {:watch-dirs ["resources/static/scss" "resources/static/vendor"]
+                            :file-patterns [#"\.scss"]
+                            :tasks ["scss once" "minify-assets"]}
+                     :js   {:watch-dirs ["resources/static/js" "resources/static/vendor"]
+                            :file-patterns [#"\.js"]
+                            :tasks ["minify-assets"]}}}
 
   :aliases {"build" ["do" ["scss" "once"] ["minify-assets"]]}
 
